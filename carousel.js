be61c24c7ed389ebcd8d82ec166ccea47518e9d9
lucas lefevre <dioburando@hotmail.fr>
2024-12-09 09:11:46 +0100

    1er commit

---
+++ carousel.js
@@ -0,0 +1,37 @@
+    document.addEventListener ("DOMContentLoaded",function() {
+    const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.carousel-button.next');
+    const prevButton = document.querySelector('.carousel-button.prev');
+    
+    let currentIndex = 0;
+    
+    function updateSlidePosition() {
+        const slideWidth = slides[0].getBoundingClientRect().width;
+        track.style.transform = 'translateX(-' + (slideWidth * currentIndex) + 'px)';
+    }
+    
+    nextButton.addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateSlidePosition();
+    });
+    
+    prevButton.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        updateSlidePosition();
+    });
+});
+
+$(document).ready(function(){
+$('.carousel-button.next').on('click', function() {
+    console.log('droite')
+});
+
+$('.carousel-button.prev').on('click', function() {
+console.log('gauche')
+});
+
+
+});
+
+
